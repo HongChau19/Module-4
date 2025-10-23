a99c81bcbc4eb3efcd47b66e7acf22a7e8014509
HongChau19 <vohongchau20@gmail.com>
2025-10-23 17:27:25 +0700

    Initial project setup

---
+++ Ex/spring-customer-management-restful/src/main/java/org/example/springcustomermanagementrestful/service/ICustomerService.java
@@ -0,0 +1,12 @@
+package org.example.springcustomermanagementrestful.service;
+
+import org.example.springcustomermanagementrestful.model.Customer;
+
+import java.util.Optional;
+
+public interface ICustomerService {
+    Iterable<Customer> findAll();
+    Optional<Customer> findById(Long id);
+    Customer save(Customer customer);
+    void remove(Long id);
+}
